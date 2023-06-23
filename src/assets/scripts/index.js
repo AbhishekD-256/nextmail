195fc593503d1d50ab87f51bef87e4f76763471d
Abhishek Dolai <itsabhi183@gmail.com>
2023-06-23 15:46:36 +0530

    add dropdown, more-options and active mail-list functionlity

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,3 +3,49 @@ function importAll(r) {
   r.keys().forEach(r);
 }
 importAll(require.context("../images/", true, /\.(png|jpe?g|gif|svg)$/));
+
+const mailList = document.querySelectorAll(".list-item");
+
+//=============== ACTIVE-MAIL =================//
+
+function activeMail(item) {
+  mailList.forEach((item) => {
+    item.classList.remove("active");
+  });
+  item.classList.add("active");
+  item.classList.remove("unread");
+}
+
+mailList.forEach((item) => {
+  item.addEventListener("click", function () {
+    activeMail(item);
+  });
+});
+
+//==================== DROPDOWN ======================//
+
+const filterMenu = document.querySelector(".filter-menu");
+const dropdown = document.querySelector(".dropdown");
+
+filterMenu.addEventListener("click", function () {
+  dropdown.classList.toggle("hide");
+});
+
+document.addEventListener("click", function (e) {
+  const element = e.target.closest(".filter-menu");
+  if (element?.classList.contains("filter-menu")) return;
+  dropdown.classList.add("hide");
+});
+
+//==================== MORE-OPTIONS ======================//
+
+const moreNav = document.querySelector(".more-nav");
+const moreBtn = document.querySelector(".js-more");
+const moreTextToggle = moreBtn.querySelector(".more-text");
+
+moreBtn.addEventListener("click", function () {
+  moreNav.classList.toggle("hide");
+  moreNav.classList.contains("hide")
+    ? (moreTextToggle.textContent = "More..")
+    : (moreTextToggle.textContent = "Less...");
+});
